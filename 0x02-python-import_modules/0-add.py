0d459a6e40e9d5d1a6a7cd2ed04eaeb4647e03d4
SpontaneousHotDog89 <martinthabang@gmail.com>
2024-03-14 15:43:26 -0700

    Add

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/bash/python3
+def add(a,b):
+    """This function will take two
+    intergers and retrn their sum
+
+    Args: a - first int
+          b - second int
+
+    Return: Return the sumof a and b
+    """
+
+    return (a+b)
